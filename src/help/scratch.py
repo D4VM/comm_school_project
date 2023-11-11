b2f9fc6f2722d06c5d32c6d94da0328f46a235d1
david <david.miru@gmail.com>
2023-11-11 11:47:14 +0400

    some testing

--- src/help/scratch.py
+++ src/help/scratch.py
@@ -1,1 +1,3 @@
-from pathlib import Path
+from tools.db import  add_to_db
+
+add_to_db('https://www.myauto.ge/ka/pr/97903590/iyideba-manqanebi-kabrioleti-bmw-325-1989-benzini-tbilisi?offerType=superVip')
