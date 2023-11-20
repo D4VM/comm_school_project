7b815df7922982dcb08d04753a7a169c5b601a26
D4VM <davit.miru@gmail.com>
2023-11-20 13:51:14 +0400

    added func descriptions

--- src/tools/op.py
+++ src/tools/op.py
@@ -1,4 +1,4 @@
-from tools.db import add_one , query_database
+from tools.db import add_one, query_database
 from tools.scrape import scrape_one, search_scrape
 from tools.utils import calculate_average
 
@@ -18,12 +18,22 @@ def scrape_and_add(url: str):
 
 
 def add_one_from_list(url):
+    """
+    Adds one by one cars to database from search URL
+    :param url:
+    :return:
+    """
     car_list = search_scrape(url)
     for car in car_list:
         add_one(car)
 
 
 def appraisal(url: str):
+    """
+    Queries Database for specific year and model, calculates averages price
+    :param url:
+    :return:
+    """
     data = scrape_one(url)
     query = query_database(data)
     average_price = calculate_average(query)
